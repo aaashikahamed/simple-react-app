d7e132bc70e3d5e991c21373df56e84594692401
aaashikahamed <ahamedaashik071@gmail.com>
2024-08-05 18:40:29 +0530

    useState
    Simple App take input from user and show it page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
+import React ,{useState} from 'react';
 import './App.css';
 
+
+
 function App() {
+
+  const [ name, setName] = useState('')
+  const [age, setAge] = useState('')
+
+  // const changeName = () => {
+  //   setName('John')
+  // }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello World</h1>
-      </header>
+   <>
+    <div className="App" >
+     
+     <h1>Hello</h1>
+      <div>
+        <label style={{padding:'10px'}}>Name</label>
+        <input type='text'  onChange={(e) => setName(e.target.value)} style={{margin:'5px'}}/> 
+      </div>
+
+      <div>
+        <label style={{padding:'10px'}}>Age</label>
+        <input type='number'  onChange={(e) => setAge(e.target.value)}  style={{margin:'5px'}}/> 
+      </div>
+
+      {/* <button onClick={changeName}>Submit</button> */}
     </div>
+
+    <div className='App'>
+      <h1>My name is {name} and I am {age} years old.</h1>
+    </div>
+   </>
   );
 }
 
